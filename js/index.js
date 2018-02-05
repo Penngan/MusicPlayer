296112cc846d4e4227a64d92c0cfee5558189c73
penngan <190875565@qq.com>
2018-02-05 23:00:39 +0800

    demo

--- js/index.js
+++ js/index.js
@@ -11,8 +11,10 @@ $('.volume').addEventListener('mouseleave',function(){
 var currentIndex = 0
 var audio = new Audio()
 audio.autoplay = true
+var musicList
 
 getMusicList(function(list){
+    musicList = list
     loadMusic(list[currentIndex])
 })
 
@@ -38,6 +40,61 @@ function loadMusic(musicObj){
     audio.src = musicObj.src
 }
 audio.ontimeupdate = function(){
-    console.log(this.currentTime)
     $('.bar .bar-now').style.width = (this.currentTime/this.duration)*100 + '%'
 }
+audio.onplay = function(){
+    clock = setInterval(function(){
+        var min_total = Math.floor(audio.duration/60)
+        var sec_total = Math.floor(audio.duration%60) + ''
+        sec_total = sec_total.length === 2?sec_total:'0' + sec_total
+        $('.musictime .alltime').innerText = min_total + ':' + sec_total
+        var min_now = Math.floor(audio.currentTime/60)
+        var sec_now = Math.floor(audio.currentTime%60) + ''
+        sec_now = sec_now.length === 2?sec_now:'0' + sec_now
+        $('.musictime .currenttime').innerText = min_now + ':' + sec_now
+    },1000)
+}
+audio.onpause = function(){
+    clearInterval(clock)
+}
+$('.control .play').onclick = function(){
+    if(audio.paused){
+        audio.play()
+        $('.play .iconfont').classList.remove('icon-play')
+        $('.play .iconfont').classList.add('icon-pause')
+    }else{
+        audio.pause()
+        $('.play .iconfont').classList.remove('icon-pause')
+        $('.play .iconfont').classList.add('icon-play')
+    }
+}
+$('.control .next').onclick = function(){
+    currentIndex++
+    currentIndex = currentIndex%musicList.length
+    loadMusic(musicList[currentIndex])
+}
+$('.control .back').onclick = function(){
+    currentIndex--
+    currentIndex = (currentIndex + musicList.length)%musicList.length
+    loadMusic(musicList[currentIndex])
+}
+$('.bar').onclick = function(e){
+    var totalwidth = getComputedStyle(this).width
+    var percent = e.offsetX/parseInt(totalwidth)
+    audio.currentTime = percent*audio.duration
+}
+// $('.voice-control ,voice-now').addEventListener('click',function(e){
+//     e.stopPropagation()
+// },false)
+
+$('.voice-control').onclick = function(e){
+    var totalheight = getComputedStyle(this).height
+    var percentvoi =  (60-e.offsetY)/parseInt(totalheight)
+    audio.volume = percentvoi
+    $('.volume .voice-now').style.height = percentvoi * 100 + '%'
+}
+    
+    
+    
+
+    
